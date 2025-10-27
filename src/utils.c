5ffbc8c7d82aa26967b9df1de17a21c3083e9b16
Lucas Maia <lucasm@posteo.de>
2025-10-27 13:24:22 +0100

    add utils.c
    add functions:
     - get_time_ms()
     - ft_usleep()
     - print_status()
     - take_chopsticks()
     - drop_chopsticks()

--- src/utils.c
+++ src/utils.c
@@ -16,3 +16,43 @@ void	ft_usleep(long long milliseconds)
 	while (get_time_ms() - start < milliseconds)
 		usleep(500);
 }
+
+void	print_status(t_philo *philo, char *status)
+{
+	long long timestamp;
+
+	pthread_mutex_lock(&philo->data->print_mutex);
+	if (!philo->data->dead_flag)
+	{
+		timestamp = get_time_ms() - philo->data->start_time;
+		printf("%lld %d %s\n", timestamp, philo->id, status);
+	}
+	pthread_mutex_unlock(&philo->data->print_mutex);
+}
+
+void	take_chopsticks(t_philo *philo)
+{
+	int	first;
+	int	second;
+
+	if (philo->left_chopstick < philo->right_chopstick)
+	{
+		first = philo->left_chopstick;
+		second = philo->right_chopstick;
+	}
+	else
+	{
+		first = philo->right_chopstick;
+		second = philo->left_chopstick;
+	}
+	pthread_mutex_lock(&philo->data->chopsticks[first]);
+	print_status(philo, "has taken a chopstick");
+	pthread_mutex_lock(&philo->data->chopsticks[second]);
+	print_status(philo, "has taken a chopstick");
+}
+
+void	drop_chopsticks(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->data->chopsticks[philo->left_chopstick]);
+	pthread_mutex_unlock(&philo->data->chopsticks[philo->right_chopstick]);
+}
