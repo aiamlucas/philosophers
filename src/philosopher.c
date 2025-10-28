c5973eb70cc944fdc2f0726fe2df7266af5c4697
Lucas Maia <lucasm@posteo.de>
2025-10-28 21:44:59 +0100

    refactor: break down philosopher_routine core logic in small functions but keeping philosopher_routine

--- src/philosopher.c
+++ src/philosopher.c
@@ -1,5 +1,40 @@
 #include "philo.h"
 
+static bool	philo_eat(t_philo *philo)
+{
+	take_chopsticks(philo);
+	if (simulation_ended(philo->data))
+	{
+		drop_chopsticks(philo);
+		return (false);
+	}
+	print_status(philo, "is eating");
+	pthread_mutex_lock(&philo->data->death_mutex);
+	philo->last_meal_time = get_time_ms();
+	philo->meals_eaten++;
+	pthread_mutex_unlock(&philo->data->death_mutex);
+	ft_usleep_check(philo->data, philo->data->time_to_eat);
+	drop_chopsticks(philo);
+	return (true);
+}
+
+static bool	philo_sleep(t_philo *philo)
+{
+	if (simulation_ended(philo->data))
+		return (false);
+	print_status(philo, "is sleeping");
+	ft_usleep_check(philo->data, philo->data->time_to_sleep);
+	return (true);
+}
+
+static bool philo_think(t_philo *philo)
+{
+	print_status(philo, "is thinking");
+	if (simulation_ended(philo->data))
+		return (false);
+	return (true);
+}
+
 void	*philosopher_routine(void *arg)
 {
 	t_philo	*philo;
@@ -9,26 +44,12 @@ void	*philosopher_routine(void *arg)
 		usleep(1000);
 	while (!simulation_ended(philo->data))
 	{
-		print_status(philo, "is thinking");
-		if (simulation_ended(philo->data))
+		if (!philo_think(philo))
 			break ;
-		take_chopsticks(philo);
-		if (simulation_ended(philo->data))
-		{
-			drop_chopsticks(philo);
+		if (!philo_eat(philo))
 			break ;
-		}
-		print_status(philo, "is eating");
-		pthread_mutex_lock(&philo->data->death_mutex);
-		philo->last_meal_time = get_time_ms();
-		philo->meals_eaten++;
-		pthread_mutex_unlock(&philo->data->death_mutex);
-		ft_usleep_check(philo->data, philo->data->time_to_eat);
-		drop_chopsticks(philo);
-		if (simulation_ended(philo->data))
+		if (!philo_sleep(philo))
 			break ;
-		print_status(philo, "is sleeping");
-		ft_usleep_check(philo->data, philo->data->time_to_sleep);
 	}
 	return (NULL);
 }
